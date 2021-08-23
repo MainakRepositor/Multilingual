36876ddd624b017bd851138b38520971e2d653c4
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-08-23 19:42:54 +0530

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,10 @@
+// C# program to print Hello World!
+using System;
+namespace HelloWorldApp {
+		class Hello {
+		  static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
